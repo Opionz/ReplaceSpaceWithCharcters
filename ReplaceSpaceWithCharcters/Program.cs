07c4e0f1db1d5294744598bbce822e9595c01040
Opionz <ope.ajayi@hotmail.com>
2021-11-05 05:02:30 +0100

    Add project files.

---
+++ ReplaceSpaceWithCharcters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReplaceSpaceWithCharcters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Put in your senctence below");
+            string sentence = Console.ReadLine();
+            string output = ReplaceSpaceInString(sentence);
+            Console.WriteLine(output);
+        }
+
+        public static string ReplaceSpaceInString(string Input)
+        {
+             Input = Input.Trim();
+
+            Input = Input.Replace(" ", "%20");
+
+            return Input;
+        }
+    }
+}
